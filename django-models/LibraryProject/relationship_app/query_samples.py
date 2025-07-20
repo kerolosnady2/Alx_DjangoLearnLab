5666ca8542f5d4d4fb60a9cb16ee01d49ac5fa0c
kerolosnady2 <kerolosnady777@gmail.com>
2025-07-20 12:39:54 +0300

    query_samples.py Updated

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,7 +7,8 @@ for book in books_by_author:
     print(book.title)
 
 # List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 for book in books_in_library:
     print(book.title)
