215c6f4fcf9c9d122b5eca8e4314b8a27f6b1f05
kerolosnady2 <kerolosnady777@gmail.com>
2025-07-20 14:47:18 +0300

    login_view, logout_view, and register_view Added for urls.py file in relationship_app dir

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import login_view, logout_view, register_view, list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from .views import register_view, list_books, LibraryDetailView
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
     path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
-    
-    # Auth URLs
-    path("login/", login_view, name="login"),
-    path("logout/", logout_view, name="logout"),
+
+    # Auth URLs using Django's built-in class-based views
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", register_view, name="register"),
 ]
+
