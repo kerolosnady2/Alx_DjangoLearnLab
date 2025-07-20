76ae5f1c319e6388a67fe3fe534432078594f298
kerolosnady2 <kerolosnady777@gmail.com>
2025-07-20 15:15:23 +0300

    UserProfile, user_passes_test, and Added urls

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views  # 👈 This is required by the checker
+from . import views
 
 urlpatterns = [
     path("books/", views.list_books, name="list_books"),
@@ -9,6 +9,10 @@ urlpatterns = [
     # Auth URLs using Django built-in views
     path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
-    path("register/", views.register_view, name="register"),  # 👈 This must say views.register_view
-]
+    path("register/", views.register_view, name="register"),
 
+    # Role-based Views
+    path("admin-role/", views.admin_view, name="admin_view"),
+    path("librarian-role/", views.librarian_view, name="librarian_view"),
+    path("member-role/", views.member_view, name="member_view"),
+]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,10 +4,34 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import Book
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
+
+# Helper functions
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "relationship_app/library_detail.html"  # Make sure this template exists
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
 # Register
