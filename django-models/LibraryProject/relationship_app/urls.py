6bf30454a7e2ce6aba330661dc48561aa56da5b3
kerolosnady2 <kerolosnady777@gmail.com>
2025-07-21 16:34:35 +0300

    imported list_books to urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,9 +1,9 @@
+from .views import list_books
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from .views import (
-    login_view, logout_view, register_view,
-    list_books, LibraryDetailView,
+    login_view, logout_view, register_view, LibraryDetailView,
     add_book, edit_book, delete_book
 )
 
