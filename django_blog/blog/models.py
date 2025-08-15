031900ecc60baac240bcf1fa79cf1f268e7ae5f7
kerolosnady2 <kerolosnady777@gmail.com>
2025-08-15 19:36:39 +0300

    Fixing some errors

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -6,12 +6,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    published_date = models.DateTimeField(auto_now_add=True)  # per instructions
     updated_at = models.DateTimeField(auto_now=True)
     tags = TaggableManager()
 
     def __str__(self):
         return self.title
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -20,6 +21,7 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
 
 class Comment(models.Model):
     post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
